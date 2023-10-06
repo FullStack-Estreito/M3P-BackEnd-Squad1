e2592aff27e2582ad8cdf1c68f8a6f4744228899
“FeReDragon” <f3030ma@gmail.com>
2023-10-06 20:02:26 -0300

    feat(context): dbcontext-model

---
+++ LabSchoolAPI/Context/DbContext.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LabSchoolAPI.Models;
+
+namespace LabSchoolAPI.Context
+{
+    public class LabSchoolContext : DbContext
+    {
+        public LabSchoolContext(DbContextOptions<LabSchoolContext> options) : base(options) { }
+
+        // DbSets para cada modelo
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+        public DbSet<AtendimentoModel> Atendimentos { get; set; }
+        public DbSet<AvaliacaoModel> Avaliacoes { get; set; }
+        public DbSet<EnderecoModel> Enderecos { get; set; }
+        public DbSet<ExercicioModel> Exercicios { get; set; }
+        public DbSet<WhiteLabelModel> WhiteLabels { get; set; }
+        public DbSet<LogModel> Logs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            // Configurações para o modelo Usuario
+            modelBuilder.Entity<UsuarioModel>()
+                .HasOne(u => u.Endereco)
+                .WithMany()
+                .HasForeignKey(u => u.EnderecoId);
+
+            modelBuilder.Entity<UsuarioModel>()
+                .HasOne(u => u.WhiteLabel)
+                .WithMany(wl => wl.Usuarios)
+                .HasForeignKey(u => u.WhiteLabelId);
+
+            // Configurações para o modelo Atendimento
+            modelBuilder.Entity<AtendimentoModel>()
+                .HasOne(a => a.Aluno)
+                .WithMany()
+                .HasForeignKey(a => a.AlunoID);
+
+            modelBuilder.Entity<AtendimentoModel>()
+                .HasOne(a => a.Pedagogo)
+                .WithMany()
+                .HasForeignKey(a => a.PedagogoID);
+
+            // Configurações para o modelo Avaliacao
+            modelBuilder.Entity<AvaliacaoModel>()
+                .HasOne(av => av.Aluno)
+                .WithMany()
+                .HasForeignKey(av => av.AlunoID);
+
+            modelBuilder.Entity<AvaliacaoModel>()
+                .HasOne(av => av.Professor)
+                .WithMany()
+                .HasForeignKey(av => av.ProfessorID);
+
+            // Configurações para o modelo Exercicio
+            modelBuilder.Entity<ExercicioModel>()
+                .HasOne(ex => ex.Aluno)
+                .WithMany()
+                .HasForeignKey(ex => ex.AlunoID);
+
+            modelBuilder.Entity<ExercicioModel>()
+                .HasOne(ex => ex.Professor)
+                .WithMany()
+                .HasForeignKey(ex => ex.ProfessorID);
+
+            // Configurações para o modelo Log
+            modelBuilder.Entity<LogModel>()
+                .HasOne(l => l.Usuario)
+                .WithMany()
+                .HasForeignKey(l => l.UsuarioID);
+        }
+    }
+}
