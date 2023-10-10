7ea986b7194a6118f661f5dd98f14cebac53ec70
“FeReDragon” <f3030ma@gmail.com>
2023-10-10 20:41:28 -0300

    fix(service): atendimento-correcao

--- LabSchoolAPI/Services/Interfaces/IAtendimentoRepository.cs
+++ LabSchoolAPI/Services/Interfaces/IAtendimentoRepository.cs
@@ -9,12 +9,11 @@ namespace LabSchoolAPI.Services.Interfaces
 {
     public interface IAtendimentoRepository
     {
-        Task<Atendimento> CreateAtendimento(AtendimentoCreateDTO atendimentoDto);
-        Task<IEnumerable<Atendimento>> GetAllAtendimentos();
-        Task<Atendimento> GetAtendimentoById(int id);
-        Task<bool> UpdateAtendimento(AtendimentoUpdateDTO atendimentoDto);
-        Task<bool> DeleteAtendimento(int id);
+        Task<Atendimento> CreateAsync(Atendimento atendimento);
+        Task<IEnumerable<Atendimento>> GetAllAsync();
+        Task<Atendimento> GetByIdAsync(int id);
+        Task UpdateAsync(Atendimento atendimento);
+        Task DeleteAsync(int id);
         Task<bool> ExistsAsync(int id);
     }
-
 }
