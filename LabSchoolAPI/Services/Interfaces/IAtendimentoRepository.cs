67922cff13f7a54ac0f705a75d5cf70823f11d56
“FeReDragon” <f3030ma@gmail.com>
2023-10-10 15:28:45 -0300

    feat(service): atendimento-service

---
+++ LabSchoolAPI/Services/Interfaces/IAtendimentoRepository.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LabSchoolAPI.DTOs;
+using LabSchoolAPIAtendimentoRepositoryodels;
+
+namespace LabSchoolAPI.Services.Interfaces
+{
+    public interface IAtendimentoRepository
+    {
+        Task<Atendimento> CreateAtendimento(AtendimentoCreateDTO atendimentoDto);
+        Task<IEnumerable<Atendimento>> GetAllAtendimentos();
+        Task<Atendimento> GetAtendimentoById(int id);
+        Task<bool> UpdateAtendimento(AtendimentoUpdateDTO atendimentoDto);
+        Task<bool> DeleteAtendimento(int id);
+    }
+}
