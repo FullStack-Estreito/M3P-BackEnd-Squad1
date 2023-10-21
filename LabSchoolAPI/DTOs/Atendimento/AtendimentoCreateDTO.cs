9ed72bde4d62b7b6bd002e196a4a900cdc569643
RogeMattos <rogemattos.rm@gmail.com>
2023-10-21 16:01:05 -0300

    Mensagens no body response -atendimento-controller

--- LabSchoolAPI/DTOs/Atendimento/AtendimentoCreateDTO.cs
+++ LabSchoolAPI/DTOs/Atendimento/AtendimentoCreateDTO.cs
@@ -5,21 +5,21 @@ namespace LabSchoolAPI.DTOs
     public class AtendimentoCreateDTO
     {
         [Required(ErrorMessage = "Campo Obrigatório")]
-        [MaxLength(10, ErrorMessage = "Digite a data nesse formato: dd-mm-aaaa")]
-        [MinLength(10, ErrorMessage = "Digite a data nesse formato: dd-mm-aaaa")]
+        [MaxLength(10, ErrorMessage = "Campo obrigatório, não deixe este campo vazio, Digite a data nesse formato: dd-mm-aaaa")]
+        [MinLength(10, ErrorMessage = "Campo obrigatório, não deixe este campo vazio, Digite a data nesse formato: dd-mm-aaaa")]
         public string DataHora { get; set; }
 
-        [Required(ErrorMessage = "Campo Obrigatório")]
-        [MaxLength(500, ErrorMessage = "Este campo aceita até 500 caracteres")]
+        [Required(ErrorMessage = "Campo Obrigatório, insira um vaor válido, não deixe este campo vazio")]
+        [MaxLength(500, ErrorMessage = "Campo Obrigatório, este campo aceita até 500 caracteres")]
         public string Descricao { get; set; }
 
         [Required]
         public bool StatusAtivo { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Campo obrigatório, coloque um valor numeral válido")]
         public int AlunoId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Campo obrigatório, coloque um valor numeral válido")]
         public int PedagogoId { get; set; }
     }
 }
--- LabSchoolAPI/DTOs/Atendimento/AtendimentoUpdateDTO.cs
+++ LabSchoolAPI/DTOs/Atendimento/AtendimentoUpdateDTO.cs
@@ -6,17 +6,22 @@ namespace LabSchoolAPI.DTOs
     {   
         public int Id { get; set;}
 
-        [MaxLength(10, ErrorMessage = "Digite a data nesse formato: dd-mm-aaaa")]
-        [MinLength(10, ErrorMessage = "Digite a data nesse formato: dd-mm-aaaa")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        [MaxLength(10, ErrorMessage = "Campo obrigatório, não deixe este campo vazio, Digite a data nesse formato: dd-mm-aaaa")]
+        [MinLength(10, ErrorMessage = "Campo obrigatório, não deixe este campo vazio, Digite a data nesse formato: dd-mm-aaaa")]
         public string DataHora { get; set; }
 
-        [MaxLength(500, ErrorMessage = "Este campo aceita até 500 caracteres")]
+        
+        [Required(ErrorMessage = "Campo Obrigatório, insira um vaor válido, não deixe este campo vazio")]
+        [MaxLength(500, ErrorMessage = "Campo Obrigatório, este campo aceita até 500 caracteres")]
         public string Descricao { get; set; }
 
         public bool StatusAtivo { get; set; }
 
+        [Required(ErrorMessage ="Campo obrigatório, coloque um valor numeral válido")]
         public int AlunoId { get; set; }
 
+        [Required(ErrorMessage ="Campo obrigatório, coloque um valor numeral válido")]
         public int PedagogoId { get; set; }
 
     }
