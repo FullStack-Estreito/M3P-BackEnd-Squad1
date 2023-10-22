29e545a40aa5e60107510bfc193365bc57b76cc5
RogeMattos <rogemattos.rm@gmail.com>
2023-10-22 16:36:16 -0300

    Correção dataannotations enderecodto

--- LabSchoolAPI/DTOs/Endereco/EnderecoUpdateDTO.cs
+++ LabSchoolAPI/DTOs/Endereco/EnderecoUpdateDTO.cs
@@ -20,8 +20,8 @@ namespace LabSchoolAPI.DTOs
         [MaxLength(100, ErrorMessage = "Campo Obrigatório, este campo aceita no máximo 100 caracteres")]
         public string Cidade { get; set; }
 
+        
         [Required(ErrorMessage = "Campo Obrigatório, este campo não pode ficar vazio")]
-        [MaxLength(100, ErrorMessage = "Campo Obrigatório, este campo aceita no máximo 100 caracteres")]
         public TipoEstado Estado { get; set; } //enum
 
         [Required(ErrorMessage = "Campo Obrigatório, este campo não pode ficar vazio")]
